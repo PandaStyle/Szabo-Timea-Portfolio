9fecd329b49cb2d309f9cb8b7bd2b12d3099c7b8
Zsolt Nemeth <furion1@gmail.com>
2013-08-06 17:06:27 +0200

    Various changes
    Back button, new page

--- client/portfolio.js
+++ client/portfolio.js
@@ -8,7 +8,7 @@ Template.kingspan.rendered = init;
 Template.valley.rendered = init;
 Template.vivienda.rendered = init;
 Template.detached.rendered = init;
-
+Template.bicycle.rendered = init;
 
 
 
@@ -16,7 +16,13 @@ Template.detached.rendered = init;
 function init(){
 	$('.flexslider').flexslider({
         useCSS: true,
-        directionNav: true
+        directionNav: true,
+        start: function(slider) {
+	    	$('.slides li img').click(function(event){
+	        	event.preventDefault();
+	        	slider.flexAnimate(slider.getTarget("next"));
+	    	});
+    	}
     });
 
     $('.pf-about').addClass("active")
